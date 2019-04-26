1d9596a7b43ac5278210b74e0ec0d0647807b907
Devansh Desai <devanshdesai29@gmail.com>
2019-04-26 00:49:30 -0400

    stuff

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
-import { AuthGuard } from './_guards/auth.guard';
+import { AuthGuard } from './guards/auth.guard';
 import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
